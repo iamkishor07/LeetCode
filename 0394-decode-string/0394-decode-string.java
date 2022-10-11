fd96240c1de8d86e97fc7c40a2084466cfa43656
Kishorkumar Dopathi <71483322+iamkishor07@users.noreply.github.com>
2022-10-11 15:30:39 +0530

    Time: 10 ms (13.45%), Space: 46.2 MB (5.42%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String p) {
+        // code here
+        Stack<Character> s=new Stack<>();
+        for(Character c:p.toCharArray()){
+            if(c!=']')s.push(c);
+            else{
+                    String str="";
+                while(!s.isEmpty() && s.peek()!='['){
+                    str=s.pop()+str;
+                }
+                s.pop();
+                 String number="";
+                while(!s.isEmpty() && s.peek() >='0' && s.peek()<='9'){
+                    number=s.pop()+number;
+                }
+                int req=Integer.parseInt(number);
+                String temp="";
+                for(int i=0;i<req;i++){
+                    temp+=str;
+                }
+                for(Character i:temp.toCharArray()){
+                    s.push(i);
+                }
+                // System.out.println(str);
+            }
+        }
+        String ans="";
+        while(!s.isEmpty()){
+            ans=s.pop()+ans;
+        }
+        return ans;
+        
+    }
+}
