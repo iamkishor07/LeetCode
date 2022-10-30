aba3055be1fed0006dd15a55e868a7a2f4b612d8
Kishorkumar Dopathi <71483322+iamkishor07@users.noreply.github.com>
2022-10-30 20:02:48 +0530

    Time: 11 ms (75.87%), Space: 44.5 MB (64.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,14 +4,12 @@ class Solution {
         int left=0,end=0,h=0;
         while(end<s.length()){
             char a=s.charAt(end);
-            map.put(a,map.getOrDefault(a,0)+1);
-            
-            while(map.get(a)>1){
-                char l=s.charAt(left);
-                map.put(l,map.get(l)-1);
-                left++;
+            if(map.containsKey(a)){
+                left=Math.max(map.get(a)+1,left);
             }
             h=Math.max(h,end-left+1);
+            map.put(a,end);
+           
             end++;
         }
         return h;
