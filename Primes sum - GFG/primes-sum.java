f3bff47b7b5dc51bc0174711104ff69f2ebead4b
Kishorkumar Dopathi <71483322+iamkishor07@users.noreply.github.com>
2022-11-13 10:41:35 +0530

    Added solution - LeetHub

---
+++ Primes sum - GFG/primes-sum.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.isSumOfTwo(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public static boolean isPrime(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    static String isSumOfTwo(int N){
+        for(int i=1;i<N;i++){
+            if(isPrime(i) && isPrime(N-i)){
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+}
